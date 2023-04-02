2ba0a4e979389576e17887a6609567ca6fb0a105
RudrarajKar69 <108626884+RudrarajKar69@users.noreply.github.com>
2023-04-02 15:55:29 +0530

    Update EndScreen.java
    Added comments

--- src/main/EndScreen.java
+++ src/main/EndScreen.java
@@ -13,103 +13,125 @@ import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 @SuppressWarnings("serial")
-public class EndScreen extends JPanel implements ActionListener{
-	
+public class EndScreen extends JPanel implements ActionListener {
+
+	// Timer for animation
 	Timer timer;
+	// Window object for key event handling
 	Window k;
-	int x = 0,command=0;
+	// command variable to keep track of user input
+	int x = 0, command = 0;
+	// DifficultyScreen object to be displayed next
 	DifficultyScreen next;
+	// Difficulty of the game that just ended
 	String difficulty;
-	
-	EndScreen(Window k,String difficulty)
-	{
-		this.difficulty=difficulty;
-		this.k=k;
+
+	// Constructor
+	EndScreen(Window k, String difficulty) {
+		// Initializing instance variables
+		this.difficulty = difficulty;
+		this.k = k;
 		this.setBackground(Color.black);
-		timer = new Timer(200,this);
+		// Starting timer
+		timer = new Timer(200, this);
 		timer.start();
 	}
-	
-	protected void paintComponent(Graphics g)
-	{
+
+	// Overriding paintComponent method to draw on JPanel
+	protected void paintComponent(Graphics g) {
 		draw(g);
 	}
-	
-	void draw(Graphics g)
-	{
-		g.setColor(new Color(199,99,255));
+
+	// Custom draw method to draw graphics on the JPanel
+	void draw(Graphics g) {
+		// Filling the background with a purple color
+		g.setColor(new Color(199, 99, 255));
 		g.fillRect(0, 0, Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT);
-		
-		g.setFont(g.getFont().deriveFont(Font.BOLD,96F));
+
+		// Drawing the main text "Game!" in bold font with size 96
+		g.setFont(g.getFont().deriveFont(Font.BOLD, 96F));
 		String text = "Game!";
-		int x=getXforCenteredText(text,g),y=(Game.SCREEN_HEIGHT)/4;
-		
-		//Shadow
+		int x = getXforCenteredText(text, g), y = (Game.SCREEN_HEIGHT) / 4;
+
+		// Drawing the shadow of the main text in gray color with an offset of 5 pixels
+		// in both x and y direction
 		g.setColor(Color.gray);
-		g.drawString(text, x+5, y+5);
-		
-		//Main text
+		g.drawString(text, x + 5, y + 5);
+
+		// Drawing the main text in white color
 		g.setColor(Color.white);
 		g.drawString(text, x, y);
-		
-		//Image
-		x=Game.SCREEN_WIDTH/2-(Game.tileSize/2);
-		y+=(Game.SCREEN_HEIGHT)/4-(Game.tileSize/2);
+
+		// Drawing an image of the player character in the center of the screen
+		x = Game.SCREEN_WIDTH / 2 - (Game.tileSize / 2);
+		y += (Game.SCREEN_HEIGHT) / 4 - (Game.tileSize / 2);
 		g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("PLAYER.png")).getImage(), x, y, null);
-		
-		//Menu
-		g.setFont(g.getFont().deriveFont(Font.BOLD,40F));
-		text="Restart";
-		x=getXforCenteredText(text,g);
-		y+=(Game.SCREEN_HEIGHT)/4;
+
+		// Drawing the "Restart" and "Quit" options in bold font with size 40
+		g.setFont(g.getFont().deriveFont(Font.BOLD, 40F));
+		text = "Restart";
+		x = getXforCenteredText(text, g);
+		y += (Game.SCREEN_HEIGHT) / 4;
 		g.drawString(text, x, y);
-		if(command==0)
-			g.drawString(">", x-(Game.SCREEN_HEIGHT)/2, y);
-		
-		text="Quit";
-		x=getXforCenteredText(text,g);
-		y+=(Game.SCREEN_HEIGHT)/6;
+		if (command == 0)
+			g.drawString(">", x - (Game.SCREEN_HEIGHT) / 2, y);
+
+		text = "Quit";
+		x = getXforCenteredText(text, g);
+		y += (Game.SCREEN_HEIGHT) / 6;
 		g.drawString(text, x, y);
-		text="Restart";
-		x=getXforCenteredText(text,g);
-		if(command==1)
-			g.drawString(">", x-(Game.SCREEN_HEIGHT)/2, y);
+		text = "Restart";
+		x = getXforCenteredText(text, g);
+		if (command == 1)
+			g.drawString(">", x - (Game.SCREEN_HEIGHT) / 2, y);
 
 	}
-	private int getXforCenteredText(String text,Graphics g2) {
+
+	// Helper method to get x coordinate for centered text
+	private int getXforCenteredText(String text, Graphics g2) {
 		int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-		int x = Game.SCREEN_WIDTH/2-length/2;
+		int x = Game.SCREEN_WIDTH / 2 - length / 2;
 		return x;
 	}
 
+	// Overriding actionPerformed method to handle
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		KeyEvent e1 = k.getkeyPressed();
-		if(e1!=null)
-		{
-			switch(e1.getKeyCode())
-			{
-			case KeyEvent.VK_UP:
-				command=0;
-				break;
-			case KeyEvent.VK_DOWN:
-				command=1;
-				break;
-			case KeyEvent.VK_ENTER:
-				if(command==1)
-					System.exit(0);
-				else if(command==0) {
-					{	
-						next=new DifficultyScreen(k,difficulty);
-						k.add(next);
-						SwingUtilities.updateComponentTreeUI(k);
-						timer.stop();
-						k.remove(this);
-					}
-				}
-				break;
-			}
-		}
-		repaint();
-	}
+    	// Get the key event from the window
+    	KeyEvent e1 = k.getkeyPressed();
+    	// Check if a key was pressed
+    	if(e1!=null)
+    	{
+	        // Check which key was pressed
+        	switch(e1.getKeyCode())
+        	{
+        	case KeyEvent.VK_UP:
+            	// Move the selection to the "Restart" option
+            	command=0;
+            	break;
+        	case KeyEvent.VK_DOWN:
+            	// Move the selection to the "Quit" option
+            	command=1;
+            	break;
+        	case KeyEvent.VK_ENTER:
+            	// Check which option is currently selected
+            	if(command==1)
+	                // If "Quit" is selected, exit the program
+	                System.exit(0);
+		else if(command==0) {
+	                // If "Restart" is selected, go back to the difficulty screen
+                	next=new DifficultyScreen(k,difficulty);
+                	k.add(next);
+                	// Update the window's UI to show the new screen
+                	SwingUtilities.updateComponentTreeUI(k);
+                	// Stop the timer
+                	timer.stop();
+                	// Remove this screen from the window
+                	k.remove(this);
+            	}
+            	break;
+        	}
+    	}
+    	// Redraw the screen
+    	repaint();
 }
